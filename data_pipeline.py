ee51b6acd78cb1c68c8f3cd82fb9d3efed8c06f5
WillChing01 <williamching2001@gmail.com>
2020-05-23 17:42:07 +0100

    Fully processed beats

--- data_pipeline.py
+++ data_pipeline.py
@@ -8,6 +8,8 @@ from keras.utils import Sequence
 sys.path.insert(0,os.getcwd())
 
 datasize=88
+sequence_length=64
+data_trans=[-3,3]
 
 class Data_Generator(Sequence):
 
@@ -16,6 +18,7 @@ class Data_Generator(Sequence):
         self.filenames=filenames[0:len(filenames)-len(filenames)%batch_size]
         self.indexes=np.arange(len(self.filenames))
         self.batch_size=batch_size
+        self.steps_per_epoch=sum([file[1] for file in self.filenames])
         self.on_epoch_end()
 
     def on_epoch_end(self):
@@ -25,12 +28,38 @@ class Data_Generator(Sequence):
         return int(len(self.filenames)/self.batch_size)
 
     def __getitem__(self,idx):
-        files=self.filenames[idx*self.batchsize:(idx+1)*self.batchsize]
+        files=self.filenames[self.indexes[idx*self.batchsize]:self.indexes[(idx+1)*self.batchsize]]
+
+        samples=sum([file[1] for file in files])
+
+        x_data=np.zeros((samples,sequence_length,datasize),dtype=np.int8)
+        y_data=np.zeros((samples,datasize),dtype=np.int8)
+
+        i=0
+        for file in files:
+            sequence=np.load(file[0])
+            for j in range(sequence.shape[0]-sequence_length):
+                sequence_x=sequence[j:j+sequence_length]
+                sequence_y=sequence[j+sequence_length]
+
+                #apply random transposition.
+                trans=np.random.randint(data_trans[0],data_trans[1]+1)
+                x_data[i]=[a[trans:datasize]+a[0:trans] for a in sequence_x]
+                y_data[i]=sequence_y[trans:datasize]+sequence_y[0:trans]
+                i+=1
+
+        return x_data,y_data
+                
 
 def process_data():
-    files=glob.glob('midi_files/*.midi')
+    files=sorted(glob.glob('midi_files/*.midi'))
+    already=sorted(glob.glob('processed_beats/*.npy'))
 
     for file in files:
+        name=file[11:-5]
+        if ('processed_beats\\'+name+'.npy') in already:
+            print("Already got file.")
+            continue
         try:
             midi=converter.parse(file)
             print("Successfully read:",file)
@@ -67,8 +96,7 @@ def process_data():
                     for i in range(duration):
                         holder[offset+i][element.pitch.midi-21]=1
 
-        name=file[11:-5]
         np.save(os.getcwd()+'\\processed_beats\\'+name,holder)
 
-if __name__=='__main__':
-    process_data()
+##if __name__=='__main__':
+##    process_data()
